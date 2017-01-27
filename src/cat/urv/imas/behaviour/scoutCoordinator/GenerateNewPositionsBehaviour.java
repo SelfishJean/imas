ccbd0612ab373ffd0ea7e0de970334e322e08792
Alex <SelfishJean@users.noreply.github.com>
2017-01-27 16:43:42 +0500

    Add files via upload

--- src/cat/urv/imas/behaviour/scoutCoordinator/GenerateNewPositionsBehaviour.java
+++ src/cat/urv/imas/behaviour/scoutCoordinator/GenerateNewPositionsBehaviour.java
@@ -38,12 +38,12 @@ public class GenerateNewPositionsBehaviour extends SimpleBehaviour {
     @Override
     public void action() { 
         System.out.println("(ScoutCoordinator) GenerationNewPositionsBehaviour starting...");
-        changeAgentsPositions();
+        changeAgentsPositions2();
 
     }
     
 
-    public void changeAgentsPositions() {
+    public void changeAgentsPositions() { // It changes positions of agents randomly.
         InfoAgent info, infoCopy;
         boolean /*is*,*/ found;
         int i,j, ri, rj, imax, jmax;
@@ -105,6 +105,92 @@ public class GenerateNewPositionsBehaviour extends SimpleBehaviour {
         }
         agent.setNewInfoAgent(newPositions);
     }
+    
+    
+    public void changeAgentsPositions2(){ // It changes positions randomly but cell by cell (moving UP, DOWN, RIGHT OR LEFT)
+        InfoAgent info, infoCopy;
+        boolean /*is*,*/ found;
+        int i,j, ri, rj, imax, jmax, movement;
+        Random rand = new Random();
+
+        Cell [][] mapa;
+        ScoutCoordinator agent = (ScoutCoordinator) this.getAgent();
+        
+        mapa = agent.getGame().getMap();
+        imax = mapa.length;
+        jmax = mapa[0].length;
+        Map listOfAgents = agent.getGame().getAgentList();
+        Set<AgentType> setOfAgents = listOfAgents.keySet();
+        HashMap newListOfAgents; // We need to create another list of agents and update it.
+        newListOfAgents = new HashMap<AgentType, List<Cell>>();
+        List<Cell> newCells; // The list of agents is a map of AgentType and list of cells.
+        StreetCell ce;
+        ArrayList<InfoAgent> newPositions;
+        newPositions = new ArrayList<InfoAgent>();
+        found = false;
+        
+        for (AgentType at : setOfAgents){
+        List<Cell> cells = (List<Cell>)listOfAgents.get(at);  
+        newCells = new ArrayList<Cell>(); // For every agentType we change the list of cells.
+        if (at.toString() == "SCOUT") 
+        {
+            //System.out.println("(GenerateNewPosition) "+at);
+            for (Cell c : cells){
+                i = c.getRow();
+                j = c.getCol();
+                //is = x[i][j].isThereAnAgent();
+                if (mapa[i][j] instanceof StreetCell) {
+                    info = ((StreetCell)mapa[i][j]).getAgent();
+                    //System.out.println(info.getAID());
+                    infoCopy = ((StreetCell)mapa[i][j]).getAgent();
+                    
+                    while (!found) {
+                        movement = Math.abs(rand.nextInt(4)) + 1; // 4 is the maximum and 1 is our minimum
+                        
+                        switch(movement)
+                        {
+                            case 1: // UP
+                                ri = i-1;
+                                rj = j;
+                                break;
+                            case 2: // DOWN
+                                ri = i+1;
+                                rj = j;
+                                break;
+                            case 3: // RIGHT
+                                ri = i;
+                                rj = j+1;
+                                break;
+                            case 4: // LEFT
+                                ri = i;
+                                rj = j-1;
+                                break;
+                            default: // This case should never happen but it is necessary to initialize ri and rj.
+                                ri = 0;
+                                rj= 0;
+                        }
+                        
+                        if (mapa[ri][rj].getCellType().toString() == "STREET" && !((StreetCell)mapa[ri][rj]).isThereAnAgent()) {
+                            found = true;
+
+                            infoCopy.setRow(ri);
+                            infoCopy.setColumn(rj);
+                            infoCopy.setPreRow(i);
+                            infoCopy.setPreColumn(j);
+                            newPositions.add(infoCopy);
+                            //System.out.println(newPositions.size());
+                            //((StreetCell)mapa[ri][rj]).addAgent(infoCopy);
+
+                        }
+                    }
+                    found = false;
+                }
+            }
+        }
+        }
+        agent.setNewInfoAgent(newPositions);
+    }
+            
     
     @Override
     public boolean done() {
